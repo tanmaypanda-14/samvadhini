f15aec2a8038107478402f0b2ce910f07855569d
uneconomicalfairy14 <87299490+uneconomicalfairy14@users.noreply.github.com>
2022-06-22 16:32:12 +0000

    edkp-admin

---
+++ react-projects/edkp-admin/src/App.js
@@ -0,0 +1,11 @@
+import Home from "./pages/home/home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-projects/edkp-admin/src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./home.scss"
+
+const Home = () => {
+    return(
+        <div className="home">
+            <h1 className='title'>ADMIN</h1>
+        </div>
+    )
+}
+
+export default Home
