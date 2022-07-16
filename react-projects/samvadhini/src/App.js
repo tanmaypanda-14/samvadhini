e75f826950b421cf1edd36abdf0efca991129610
uneconomicalfairy14 <87299490+uneconomicalfairy14@users.noreply.github.com>
2022-07-16 04:57:28 +0000

    10:27 Co-authored-by: Divyansh Sharma <div360@users.noreply.github.com>

--- react-projects/samvadhini/src/App.js
+++ react-projects/samvadhini/src/App.js
@@ -3,8 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Model from './pages/Model/Model';
 import Thanks from './pages/TU/TU';
+
 const App =()=>{
+
+
   return(
+    <div className="App">
     <BrowserRouter>
     <Routes>
       <Route exact path ='/' element={<Home/>}/>
@@ -12,6 +16,7 @@ const App =()=>{
       <Route exact path= '/thankyou' element={<Thanks />}/>
     </Routes>
     </BrowserRouter>
+    </div>
   )
 }
 export default App
