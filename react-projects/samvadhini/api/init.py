8f308536ef4db068d9b4ebf74fe88a63b5372c16
uneconomicalfairy14 <87299490+uneconomicalfairy14@users.noreply.github.com>
2022-07-25 14:39:28 +0000

    20.09

--- react-projects/samvadhini/api/init.py
+++ react-projects/samvadhini/api/init.py
@@ -5,16 +5,17 @@ from haystack.nodes import TfidfRetriever
 from haystack.pipelines import ExtractiveQAPipeline
 from flask import Flask, make_response, request
 from googletrans import Translator
-import requests
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
+
 def gen_result(text):
     # text = "ळ " + text
     translator = Translator()
+    detect_lang = translator.detect(text)
     dt1 = translator.translate(text, dest="en")
     translated_query = dt1.text
 
@@ -23,7 +24,16 @@ def gen_result(text):
         query=translated_query,
         params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}},
     )
-    return prediction
+    print(type(prediction))
+    print(prediction)
+    documents = prediction["documents"]
+    final_response = []
+    for doc in documents:
+        question = translator.translate(doc.content, dest=detect_lang.lang).text
+        answer = translator.translate(doc.meta["answer"], dest=detect_lang.lang).text
+        final_response.append({"question": question, "answer": answer})
+
+    return {"documents": final_response}
 
 
 debug = True  # set to True to see the request and response
@@ -49,12 +59,7 @@ def query():
 if __name__ == "__main__":
     document_store = InMemoryDocumentStore()
     retriever = TfidfRetriever(document_store=document_store)
-
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
-    temp = requests.get(
-        "https://raw.githubusercontent.com/uneconomicalfairy14/samvadhini/master/api/faq.csv"
-    )
-    open("faq.csv", "wb").write(temp.content)
+    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=False)
     df = pd.read_csv("faq.csv")
     df.fillna(value="", inplace=True)
     df["question"] = df["question"].apply(lambda x: x.strip())
@@ -66,4 +71,5 @@ if __name__ == "__main__":
     pipe = ExtractiveQAPipeline(reader, retriever)
     # temp = trans()
     # print(temp)
+    app.config['JSON_AS_ASCII'] = False
     app.run(debug=True, host="0.0.0.0")
